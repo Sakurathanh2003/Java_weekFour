c91db30d7e7b7f85b9a67337ef14c8746f2cde70
Sakurathanh2003 <96622230+Sakurathanh2003@users.noreply.github.com>
2022-11-08 14:06:03 +0700

    update bai4

--- src/Bai4.java
+++ src/Bai4.java
@@ -1,2 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.lang.String;
+
 public class Bai4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        a = a.trim();
+        a = a.toLowerCase();
+
+        boolean space = false;
+        boolean firstLetter = false;
+
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) >= '0' && a.charAt(i) <= '9') {
+                continue;
+            }
+            if (a.charAt(i) == ' ' && space) continue;
+            if (a.charAt(i) == ' ' && !space) {
+                space = true;
+                System.out.print(a.charAt(i));
+                continue;
+            }
+
+            if (space || firstLetter == false) {
+                System.out.print(a.toUpperCase().charAt(i));
+                firstLetter = true;
+            } else {
+                System.out.print(a.charAt(i));
+            }
+            
+            space = false;
+        }
+    }
 }
