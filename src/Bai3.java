4e0e8db23a6004878a87a16ce141e9e412e0aca6
Sakurathanh2003 <96622230+Sakurathanh2003@users.noreply.github.com>
2022-11-08 13:32:59 +0700

    update Bai3

--- src/Bai3.java
+++ src/Bai3.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+import java.lang.String;
+
 public class Bai3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+
+        int dem = 0;
+        int so = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) >= '0' && a.charAt(i) <= '9') {
+                dem += a.charAt(i) - '0';
+                so++;
+            }
+        }
+
+        if (dem == 0) {
+            System.out.print("false");
+            return;
+        }
+
+        double ans = (double)dem / so;
+        ans = lamtron(ans);
+
+        System.out.println("true");
+        System.out.print(ans);
+    }
+
+    public static double lamtron(double a) {
+        a = a * 1000;
+        a = Math.round(a);
+        a = a / 1000;
+        return a;
+    }
 }
