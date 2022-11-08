0c41bcc44afe25df7e1b8621eaef6377bb2ddd04
Sakurathanh2003 <96622230+Sakurathanh2003@users.noreply.github.com>
2022-11-08 13:03:20 +0700

    create base

---
+++ src/Bai1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.nextLine();
+
+
+    }
+
+    public static void sortString(String a) {
+
+    }
+}
